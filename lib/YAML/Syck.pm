4bd6180173de4e09d6fd79c1d981c932b0f7454b
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2006-01-08 19:37:14 +0000

    copy YAML to JSON

---
+++ lib/YAML/Syck.pm
@@ -0,0 +1,27 @@
+package YAML::Syck;
+use strict;
+use vars qw( @ISA @EXPORT $VERSION );
+
+use 5.003;
+use DynaLoader;
+
+$VERSION = '0.10';
+@EXPORT  = qw( Dump Load DumpFile LoadFile );
+@ISA     = qw( Exporter DynaLoader );
+
+sub DumpFile {
+    my $file = shift;
+    local *FH;
+    open FH, "> $file" or die "Cannot write to $file: $!";
+    print FH Dump($_[0]);
+}
+sub LoadFile {
+    my $file = shift;
+    local *FH;
+    open FH, "< $file" or die "Cannot read from $file: $!";
+    Load(do { local $/; <FH> })
+}
+
+__PACKAGE__->bootstrap;
+
+1;
